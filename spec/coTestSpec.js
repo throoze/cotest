bc05441627960aaa15e3c1476fc22fe0280c5a85
Victor De Ponte <victor.deponte@neoris.com>
2019-04-04 06:54:50 -0300

    Created specs for Product
    Product description in tests implemented according to requirements

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -1,15 +1,20 @@
-const expect = require('chai').expect;
+// const expect = require('chai').expect;
+// const CarInsurance = coTest.CarInsurance;
 
-const coTest = require('../src/coTest');
-const CarInsurance = coTest.CarInsurance;
-const Product = coTest.Product;
+const test = (name, path) => {
+  describe(name, () => {
+      require(path);
+  });
+}
 
 describe("Co Test", function() {
 
-  it("should foo", function() {
-    const coTest = new CarInsurance([ new Product("foo", 0, 0) ]);
-    const products = coTest.updatePrice();
-    expect(products[0].name).equal("fixme");
-  });
+  test("Products", './productsSpec.js');
+
+  // it("should foo", function() {
+  //   const coTest = new CarInsurance([ new Product("foo", 0, 0) ]);
+  //   const products = coTest.updatePrice();
+  //   expect(products[0].name).equal("fixme");
+  // });
 
 });
